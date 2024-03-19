fa06d5658a2fb83e549f6f698155898418dccb84
iamsujonsheikh <sujonsheikh.in@gmail.com>
2024-03-19 17:11:42 +0600

    done cart click

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,46 +1,56 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState } from "react";
 import Moon from "./assets/icons/moon.svg";
 import Logo from "./assets/logo.svg";
 import Ring from "./assets/ring.svg";
 import Cart from "./assets/shopping-cart.svg";
+import CartDetails from "./cine/CartDetails";
 
 const Header = () => {
-  return (
-    <header>
-      <nav className="container flex items-center justify-between space-x-10 py-6">
-        <a href="index.html">
-          <img src={Logo} width="139" height="26" alt="logo" />
-        </a>
+  const [showDetails, setShowDetails] = useState(false);
 
-        <ul className="flex items-center space-x-5">
-          <li>
-            <a
-              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
-              href="#"
-            >
-              <img src={Ring} width="24" height="24" alt="ring" />
-            </a>
-          </li>
-          <li>
-            <a
-              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
-              href="#"
-            >
-              <img src={Moon} width="24" height="24" alt="moon" />
-            </a>
-          </li>
-          <li>
-            <a
-              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
-              href="#"
-            >
-              <img src={Cart} width="24" height="24" alt="cart" />
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+  const handleCartShow = () => {
+    setShowDetails(true);
+  };
+  return (
+    <>
+      {showDetails && <CartDetails />}
+      <header>
+        <nav className="container flex items-center justify-between space-x-10 py-6">
+          <a href="index.html">
+            <img src={Logo} width="139" height="26" alt="logo" />
+          </a>
+
+          <ul className="flex items-center space-x-5">
+            <li>
+              <a
+                className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+                href="#"
+              >
+                <img src={Ring} width="24" height="24" alt="ring" />
+              </a>
+            </li>
+            <li>
+              <a
+                className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+                href="#"
+              >
+                <img src={Moon} width="24" height="24" alt="moon" />
+              </a>
+            </li>
+            <li>
+              <a
+                onClick={handleCartShow}
+                className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+                href="#"
+              >
+                <img src={Cart} width="24" height="24" alt="cart" />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 };
 
--- src/cine/MovieDetailsModal.jsx
+++ src/cine/MovieDetailsModal.jsx
@@ -11,7 +11,7 @@ const MovieDetailsModal = ({ movie, onClose }) => {
           <img
             className="sm:order-2 w-full object-cover h-full max-sm:max-h-[300px]"
             src={getImageUrl(movie.cover)}
-            alt=""
+            alt={movie.title}
           />
           <div className="p-5 lg:p-11">
             <div className="">
